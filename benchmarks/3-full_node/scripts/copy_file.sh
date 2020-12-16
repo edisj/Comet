151332dc663838d0fadee9189e91dde3add4ba10
edisj <ejakupov@asu.edu>
2020-12-16 05:05:53 -0700

    new scripts

---
+++ benchmarks/3-full_node/scripts/copy_file.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --nodes=1  # number of nodes
+#SBATCH --ntasks-per-node=1  # number of cores
+#SBATCH --partition=compute       # partition
+#SBATCH -e %j.err # file to save job's STDERR (%j = JobId)
+#SBATCH -o %j.out # file to save job's STDOUT (%j = JobId)
+#SBATCH --export=ALL
+#SBATCH -t 00:20:00   # time in hh:mm:ss
+#SBATCH --mail-type=ALL # Send an e-mail when a job starts, stops, or fails
+#SBATCH --mail-user=ejakupov@asu.edu # Mail-to address
+
+#echo commands to stdout
+set -x
+
+sbatch --dependency=$SLURM_JOB_ID $1 $2 $3
+
+TEST_TOP=/oasis/projects/nsf/azs119/edisj/Comet/datafiles/YiiP_system.pdb
+TEST_TRAJ=/oasis/projects/nsf/azs119/edisj/Comet/datafiles/YiiP_system_9ns_center100x.h5md
+
+testdir=/oasis/scratch/comet/edisj/temp_project/benchmarking/$SLURM_JOB_ID
+mkdir -p $testdir
+time cp $TEST_TOP $testdir
+time cp $TEST_TRAJ $testdir
