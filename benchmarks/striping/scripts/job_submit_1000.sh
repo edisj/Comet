30299a53f5ff3a7f6adec07365f3989eb97c21f3
edisj <>
2021-01-20 11:04:45 -0700

    fixed bug

--- benchmarks/striping/scripts/job_submit_1000.sh
+++ benchmarks/striping/scripts/job_submit_1000.sh
@@ -5,8 +5,8 @@ cd /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/striping/results/$1/slurm_o
 
 for repeat in 1 2 3
 do
-    sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/striping/scripts/copy_file_1000.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/striping/scripts/1node1.sh $1 $repeat
-    sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/striping/scripts/copy_file_1000.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/striping/scripts/1node24.sh $1 $repeat
-    sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/striping/scripts/copy_file_1000.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/striping/scripts/2node48.sh $1 $repeat
-    sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/striping/scripts/copy_file_1000.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/striping/scripts/4node96.sh $1 $repeat
+    sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/striping/scripts/copy_file_1000.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/striping/scripts/1node1_1000.sh $1 $repeat
+    sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/striping/scripts/copy_file_1000.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/striping/scripts/1node24_1000.sh $1 $repeat
+    sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/striping/scripts/copy_file_1000.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/striping/scripts/2node48_1000.sh $1 $repeat
+    sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/striping/scripts/copy_file_1000.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/striping/scripts/4node96_1000.sh $1 $repeat
 done
