fba428267b4d1a3a348bf9c92afd858ab8caebc3
edisj <>
2021-02-09 22:33:48 -0700

    commented out a couple lines

--- benchmarks/h5py/scripts/job_submit.sh
+++ benchmarks/h5py/scripts/job_submit.sh
@@ -9,7 +9,7 @@ do
     sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/h5py/scripts/copy_file.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/h5py/scripts/1node24.sh $1 $repeat
     sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/h5py/scripts/copy_file.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/h5py/scripts/2node48.sh $1 $repeat
     sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/h5py/scripts/copy_file.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/h5py/scripts/4node96.sh $1 $repeat
-    sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/h5py/scripts/copy_file.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/h5py/scripts/6node144.sh $1 $repeat
+    #sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/h5py/scripts/copy_file.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/h5py/scripts/6node144.sh $1 $repeat
     #sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/h5py/scripts/copy_file.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/h5py/scripts/8node192.sh $1 $repeat
     #sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/h5py/scripts/copy_file.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/h5py/scripts/12node288.sh $1 $repeat
     #sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/h5py/scripts/copy_file.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/h5py/scripts/16node384.sh $1 $repeat
--- benchmarks/h5py/scripts/job_submit2.sh
+++ benchmarks/h5py/scripts/job_submit2.sh
@@ -8,5 +8,5 @@ do
     sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/h5py/scripts/copy_file.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/h5py/scripts/1node12.sh $1 $repeat
     sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/h5py/scripts/copy_file.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/h5py/scripts/2node36.sh $1 $repeat
     sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/h5py/scripts/copy_file.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/h5py/scripts/4node84.sh $1 $repeat
-    sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/h5py/scripts/copy_file.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/h5py/scripts/6node132.sh $1 $repeat
+    #sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/h5py/scripts/copy_file.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/h5py/scripts/6node132.sh $1 $repeat
 done
