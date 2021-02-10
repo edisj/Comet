08a2a4b5372091e3a66c0a7e1697934632d6a9fc
edisj <>
2021-02-10 02:10:02 -0700

    changed scripts

--- benchmarks/vanilla/scripts/4node84.sh
+++ benchmarks/vanilla/scripts/4node84.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --nodes=6  # number of nodes
+#SBATCH --nodes=4  # number of nodes
 #SBATCH --ntasks-per-node=24  # number of cores
 #SBATCH --partition=compute       # partition
-#SBATCH -e 6node120.%j.err # file to save job's STDERR (%j = JobId)
-#SBATCH -o 6node120.%j.out # file to save job's STDOUT (%j = JobId)
+#SBATCH -e 4node84.%j.err # file to save job's STDERR (%j = JobId)
+#SBATCH -o 4node84.%j.out # file to save job's STDOUT (%j = JobId)
 #SBATCH -t 02:00:00   # time in hh:mm:ss
 #SBATCH --mail-type=ALL # Send an e-mail when a job starts, stops, or fails
 #SBATCH --mail-user=ejakupov@asu.edu # Mail-to address
@@ -21,6 +21,6 @@ testdir=/oasis/scratch/comet/edisj/temp_project/benchmarking/$SLURM_JOB_DEPENDEN
 
 export OMP_NUM_THREADS=1
 
-time ibrun -np 120 python -W ignore /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/vanilla/scripts/benchmark.py $testdir/YiiP_system.pdb $testdir/YiiP_system_9ns_center100x.h5md $1/6node_$2
+time ibrun -np 84 python -W ignore /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/vanilla/scripts/benchmark.py $testdir/YiiP_system.pdb $testdir/YiiP_system_9ns_center100x.h5md $1/4node_$2
 
 rm -r $testdir
