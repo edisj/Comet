e6544c392248450c11eb56160d1c9f80e9c4c2af
edisj <>
2021-02-10 02:17:12 -0700

    24

--- benchmarks/vanilla_stripe/scripts/24/job_submit2.sh
+++ benchmarks/vanilla_stripe/scripts/24/job_submit2.sh
@@ -8,5 +8,5 @@ do
     sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/vanilla_stripe/scripts/24/copy_file.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/vanilla_stripe/scripts/24/1node12.sh $1 $repeat
     sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/vanilla_stripe/scripts/24/copy_file.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/vanilla_stripe/scripts/24/2node36.sh $1 $repeat
     sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/vanilla_stripe/scripts/24/copy_file.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/vanilla_stripe/scripts/24/4node84.sh $1 $repeat
-    sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/vanilla_stripe/scripts/copy_file.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/vanilla_stripe/scripts/6node132.sh $1 $repeat
+    sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/vanilla_stripe/scripts/24/copy_file.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/vanilla_stripe/scripts/24/6node132.sh $1 $repeat
 done
