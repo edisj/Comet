ea0f188a3bbb731d85f790b4ecb0736d7eb44d7c
edisj <>
2021-02-17 03:42:59 -0700

    fixed file location

--- benchmarks/Khosh_comparison/scripts/copy_file.sh
+++ benchmarks/Khosh_comparison/scripts/copy_file.sh
@@ -7,8 +7,8 @@
 
 sbatch --dependency=$SLURM_JOB_ID $1 $2 $3
 
-TEST_TOP=/oasis/scratch/comet/edisj/temp_project/benchmarking/datafiles/small_top.pdb
-TEST_TRAJ=/oasis/scratch/comet/edisj/temp_project/benchmarking/datafiles/small_traj.h5md
+TEST_TOP=/oasis/projects/nsf/azs119/edisj/Comet/datafiles/small_top.pdb
+TEST_TRAJ=/oasis/projects/nsf/azs119/edisj/Comet/datafiles/small_traj.h5md
 
 testdir=/oasis/scratch/comet/edisj/temp_project/benchmarking/$SLURM_JOB_ID
 mkdir -p $testdir
