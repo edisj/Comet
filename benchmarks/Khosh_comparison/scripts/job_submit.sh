799fb897bb69557a9ea8ded77c0f64a5bb530acd
edisj <>
2021-02-17 01:22:46 -0700

    khosh comparison

---
+++ benchmarks/Khosh_comparison/scripts/job_submit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/Khosh_comparison/results/$1/slurm_output
+cd /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/Khosh_comparison/results/$1/slurm_output
+
+for repeat in 1 2 3
+do
+    sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/Khosh_comparison/scripts/copy_file.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/Khosh_comparison/scripts/1node1.sh $1 $repeat
+    sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/Khosh_comparison/scripts/copy_file.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/Khosh_comparison/scripts/1node24.sh $1 $repeat
+    sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/Khosh_comparison/scripts/copy_file.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/Khosh_comparison/scripts/2node48.sh $1 $repeat
+    sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/Khosh_comparison/scripts/copy_file.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/Khosh_comparison/scripts/4node96.sh $1 $repeat
+    #sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/Khosh_comparison/scripts/copy_file.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/Khosh_comparison/scripts/6node144.sh $1 $repeat
+    #sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/Khosh_comparison/scripts/copy_file.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/Khosh_comparison/scripts/8node192.sh $1 $repeat
+    #sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/Khosh_comparison/scripts/copy_file.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/Khosh_comparison/scripts/12node288.sh $1 $repeat
+    #sbatch /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/Khosh_comparison/scripts/copy_file.sh /oasis/projects/nsf/azs119/edisj/Comet/benchmarks/Khosh_comparison/scripts/16node384.sh $1 $repeat
+done
